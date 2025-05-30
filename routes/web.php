36d39aff3fac3764b5e1fe9e6ae8499f760b4d24
Marcius Gabriel <105213014+MarciusGabriel@users.noreply.github.com>
2025-05-29 22:53:03 -0300

    jair

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+Route::get('/', [EventController::class,'index'] );
+Route::get('/events/create', [EventController::class,'create'] );
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/contact', function () {
+    return view('contact');
 });
+
